8e9b96d5b2f7596fdab9970c383c8fe25512f33a
Prince Mittal <mittalprince2005@gmail.com>
2025-07-05 14:29:28 +0530

    Updated terra-automate.py

--- terra-automate.py
+++ terra-automate.py
@@ -15,13 +15,13 @@ def terraform_command_live(command):
     process.wait()
     print(f"\n Command finished with exit code: {process.returncode}")
 
-directory = "/Users/princemittal/terraform/variables"
+directory = "/Users/princemittal/terraform/variables" # copy path of terraform directory to automate
 
-# print(" Initializing Terraform...")
-# terraform_command_live(f"terraform -chdir={directory} init")
+print(" Initializing Terraform...")
+terraform_command_live(f"terraform -chdir={directory} init")
 
-# print("\n Applying Terraform...")
-# terraform_command_live(f"terraform -chdir={directory} apply -auto-approve")
+print("\n Applying Terraform...")
+terraform_command_live(f"terraform -chdir={directory} apply -auto-approve")
 
-print("\n Destroying Terraform resources...")
-terraform_command_live(f"terraform -chdir={directory} destroy -auto-approve")
+# print("\n Destroying Terraform resources...")
+# terraform_command_live(f"terraform -chdir={directory} destroy -auto-approve")
